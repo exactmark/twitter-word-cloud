afd27674b45983bea06ac666091f5fcf853408b4
Mark Fowler <exactmark@gmail.com>
2020-04-21 16:20:47 -0400

    Added compression to pickling.
    Pickled scrape size has been reduced by around 75%

--- pickle_api.py
+++ pickle_api.py
@@ -1,13 +1,14 @@
 
 import pickle
+import bz2
 
 def store_pickle(pickled_item,pickled_path):
-    pickle_file = open(pickled_path,'ab')
-    pickle.dump(pickled_item,pickle_file)
-    pickle_file.close()
-    
+    bz_file = bz2.BZ2File(pickled_path,'w')
+    pickle.dump(pickled_item,bz_file)
+    bz_file.close()
+
 def get_pickle(pickled_path):
-    pickled_file = open(pickled_path,'rb')
+    pickled_file = bz2.BZ2File(pickled_path,'rb')
     pickled_item = pickle.load(pickled_file)
-    pickled_file.close
+    pickled_file.close()
     return pickled_item
